657c23233b5faffaacf27f093bf76dba65275097
BSipka <sipka.bogdan90@gmail.com>
2024-02-21 15:02:40 +0100

    Add the error messages to login form

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,17 +1,39 @@
 import React from "react";
-import { Form } from "react-router-dom";
+import {
+    Form,
+    useActionData,
+    useNavigation,
+    useSearchParams,
+} from "react-router-dom";
 
 export default function AuthForm() {
+    const data = useActionData();
+    const navigation = useNavigation();
+
+    const [searchParams] = useSearchParams();
+    const isLogin = searchParams.get("mode") === "login";
+
+    const isSubmiting = navigation.state === "submitting";
     return (
         <>
             <Form method="post">
+                {data && data.errors && (
+                    <ul>
+                        {Object.values(data.errors).map((err) => {
+                            <li key={err}>{err}</li>;
+                        })}
+                    </ul>
+                )}
+                {data && data.message && <p>{data.message}</p>}
                 <label>Email</label>
                 <input type="email" name="email" required />
 
                 <label>Password</label>
                 <input type="password" name="password" required />
 
-                <button>Login</button>
+                <button disabled={isSubmiting}>
+                    {isSubmiting ? "Submiting ..." : "Login"}
+                </button>
             </Form>
         </>
     );
